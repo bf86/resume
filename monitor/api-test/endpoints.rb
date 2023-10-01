d93d487059393d53629c440c7fc75885d5876aa8
bf86 <brian.g.fink@gmail.com>
2023-10-01 17:03:51 -0400

    DRY endpoints.rb

--- monitor/api-test/endpoints.rb
+++ monitor/api-test/endpoints.rb
@@ -3,7 +3,17 @@ require 'net/http'
 require 'rspec'
 require 'uri'
 
-API_URI = 'https://brian86.dev/api/pg/apps'
+API_URI = 'https://brian86.dev/api/pg'
+
+def get_response_test(uri, keys)
+  res = Net::HTTP.get_response(uri)
+  body = res.body
+  data = JSON.parse(body)
+  all_match = data.all? do |hash|
+    hash.keys.sort() == keys.sort()
+  end
+  expect(all_match).to eq(true)
+end
 
 describe 'API' do
   it 'Returns a list of apps' do
@@ -17,14 +27,8 @@ describe 'API' do
       'role',
       'description'
     ]
-    uri = URI(API_URI)
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/apps")
+    get_response_test(uri, keys)
   end
 
   it 'Returns a list of projects' do
@@ -34,61 +38,31 @@ describe 'API' do
       'role',
       'organization'
     ]
-    uri = URI('https://brian86.dev/api/pg/projects')
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/projects")
+    get_response_test(uri, keys)
   end
 
   it 'Returns a list of skills' do
     keys = ['type', 'name']
-    uri = URI('https://brian86.dev/api/pg/skills')
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/skills")
+    get_response_test(uri, keys)
   end
 
   it 'Returns a list of skill_types' do
     keys = ['name']
-    uri = URI('https://brian86.dev/api/pg/skill-types')
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/skill-types")
+    get_response_test(uri, keys)
   end
 
   it 'Returns a list of titles' do
     keys = ['title', 'internal_title', 'company']
-    uri = URI('https://brian86.dev/api/pg/titles')
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/titles")
+    get_response_test(uri, keys)
   end
 
   it 'Returns a list of education' do
     keys = ['institution', 'degree', 'field', 'honors']
-    uri = URI('https://brian86.dev/api/pg/education')
-    res = Net::HTTP.get_response(uri)
-    body = res.body
-    data = JSON.parse(body)
-    all_match = data.all? do |hash|
-      hash.keys.sort() == keys.sort()
-    end
-    expect(all_match).to eq(true)
+    uri = URI("#{API_URI}/education")
+    get_response_test(uri, keys)
   end
 end
