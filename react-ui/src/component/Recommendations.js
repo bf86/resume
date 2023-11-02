3d390cb59fbf7bf31b4fcf26fa0939038f41eccb
bf86 <brian.g.fink@gmail.com>
2023-11-02 09:26:03 -0400

    Fix recommendations mobile layout some more

--- react-ui/src/component/Recommendations.js
+++ react-ui/src/component/Recommendations.js
@@ -28,7 +28,7 @@ function Recommendations() {
               return ( <>
                 <tr><th><h4>{recommendation.source}</h4></th></tr>
                 <tr><th>Title</th><td>{recommendation.title}</td></tr>
-                <tr><th>See At</th><td>{recommendation.url}</td></tr>
+                <tr><th>View</th><a href={recommendation.url}>LinkedIn</a></tr>
                 <tr><th>Transcript</th><td>"{recommendation.transcript}"</td></tr>
               </> )
             })}
