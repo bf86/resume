35e2e65c366c7e71c1d7c8ba5d50b9ee47e4e975
bf86 <brian.g.fink@gmail.com>
2023-11-25 20:32:23 -0500

    Use component names instead of numbers as accordion event keys

--- react-ui/src/component/Resume.js
+++ react-ui/src/component/Resume.js
@@ -16,43 +16,43 @@ function Resume() {
       <Container>
         <p class="display-6">Resume</p>
           <Accordion>
-            <Accordion.Item eventKey="0">
+            <Accordion.Item eventKey="Skills">
               <Accordion.Header>Skills and Experience</Accordion.Header>
               <Accordion.Body>
                 <Skills />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="1">
+            <Accordion.Item eventKey="Titles">
               <Accordion.Header>Titles</Accordion.Header>
               <Accordion.Body>
                 <Titles />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="2">
+            <Accordion.Item eventKey="Apps">
               <Accordion.Header>Apps</Accordion.Header>
               <Accordion.Body>
                 <Apps />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="3">
+            <Accordion.Item eventKey="Projects">
               <Accordion.Header>Projects</Accordion.Header>
               <Accordion.Body>
                 <Projects />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="4">
+            <Accordion.Item eventKey="Education">
               <Accordion.Header>Education</Accordion.Header>
               <Accordion.Body>
                 <Education />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="5">
+            <Accordion.Item eventKey="Recommendations">
               <Accordion.Header>Recommendations</Accordion.Header>
               <Accordion.Body>
                 <Recommendations />
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="6">
+            <Accordion.Item eventKey="FAQ">
               <Accordion.Header>FAQ</Accordion.Header>
               <Accordion.Body>
                 <Faq />
