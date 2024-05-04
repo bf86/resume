e442d5bd46509e39fd89b0e089850f3a1651ac12
bf86 <brian.g.fink@gmail.com>
2024-05-04 14:59:32 -0400

    Remove SSH from provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Install Docker
-ssh $RESUME_USER@$RESUME_HOST "sudo snap install docker"
-
-# Install ncdu
-ssh $RESUME_USER@$RESUME_HOST "sudo apt-get install ncdu"
+sudo apt-get update
+sudo snap install docker
+sudo apt-get install ncdu
 
 # I thought there would be more steps lol
 # Good problem to have XD
