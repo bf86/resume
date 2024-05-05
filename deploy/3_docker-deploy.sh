8a5eab0d5fa8a577a1fe0bfd317269636ddfe569
bf86 <brian.g.fink@gmail.com>
2024-05-05 08:22:39 -0400

    Allow HTTP in lower environments with variable compose file configs

--- deploy/3_docker-deploy.sh
+++ deploy/3_docker-deploy.sh
@@ -7,15 +7,19 @@
 ## Deploy host / user
 deploy_host=""
 deploy_user=""
+compose_file=""
 if [ "$1" = "prd" ]; then
   deploy_host=$RESUME_HOST
   deploy_user=$RESUME_USER
+  compose_file="production.yml"
 elif [ "$1" = "aws" ]; then
     deploy_host=$RESUME_IP_AWS_1
     deploy_user=$RESUME_USER_AWS
+    compose_file="development.yml"
 else
   deploy_host=$RESUME_HOST_STG
   deploy_user=$RESUME_USER
+  compose_file="production.yml"
 fi
 echo "Deploying to $deploy_host as $deploy_user"
 
@@ -24,7 +28,7 @@ containers="api db cache nginx monitor"
 
 # Vars
 ssh_cmd="ssh $deploy_user@$deploy_host"
-compose_cmd="sudo docker-compose -f /home/$deploy_user/resume/compose/production.yml"
+compose_cmd="sudo docker-compose -f /home/$deploy_user/resume/compose/$compose_file"
 
 # Deploy
 $ssh_cmd $compose_cmd stop $containers
